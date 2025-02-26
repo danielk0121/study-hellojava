6e8af06e841f6260d1309325399bce8d5e0a16f4
danielk0121 <maedk10@gmail.com>
2025-02-27 05:35:44 +0900

    json 테스트 코드 전체 수정, gson 추가

--- src/test/java/dev/dk/hellojava/json/JsonTest.java
+++ src/test/java/dev/dk/hellojava/json/JsonTest.java
@@ -3,13 +3,16 @@ package dev.dk.hellojava.json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,56 +21,138 @@ import java.util.List;
 public class JsonTest {
 
     /**
-     * json object to string
-     * json string to object
-     * json array to string
-     * json string to array
+     * root 가 객체인 json 처리
+     *   객체 to json 문자열
+     *   json 문자열 to 객체
      *
-     * 잭슨 jsonObject 부터 시작
-     * 문자열 역직렬화 부터, 직렬화
-     * 문자열, 샘플 객체 준비
+     * root 가 배열인 json 처리
+     *   배열 to json 문자열
+     *   json 문자열 to 배열
      *
+     * 1. 잭슨 jsonObject 사용
+     * 2. GSON 사용
+     * 3. 소스포지 json 사용 => 다음에
+     *
+     * 주의사항
      * 검증 코드 작성
-     * root 객체 처리
-     * root 배열 처리
      * root 배열 역직렬화 시 자바 타입 경고 처리
-     *
-     * GSON 동일 처리
-     * 소스포지 json 동일 처리
      */
 
     @Test
-    void test_ObjectMapper() throws JsonProcessingException {
+    void test_Gson_object() {
+        //객체 to json 문자열, 반대로
+        //
+        //요약
+        //Gson gson = new Gson()
+        //jsonString = gson.toJson(data)
+        //data = gson.fromJson(rawString, Data.class) //Class 사용
+        //data = gson.fromJson(rawString, new TypeToken<Data>(){}.getType()) //제네릭 타입 명시
+        //data = gson.fromJson(rawString, new TypeToken<>(){}) //제네릭 타입 생략
+
+        Gson gson = new Gson();
+        Data data = createSampleData();
+        String rawString = "{\"num\":10,\"str\":\"dataHello\",\"innerData\":{\"id\":1,\"innerNum\":10,\"innerStr\":\"innerDataHello\"},\"numList\":[10,20],\"strList\":[\"a1\",\"a2\"],\"innerDataList\":[{\"id\":2,\"innerNum\":20,\"innerStr\":\"bb2\"}]}";
+
+        String jsonString = gson.toJson(data);
+        log.info("jsonString={}", jsonString);
+
+        Data readData1 = gson.fromJson(rawString, Data.class);
+        Data readData2 = gson.fromJson(rawString, new TypeToken<Data>() {});
+        Data readData3 = gson.fromJson(rawString, new TypeToken<Data>() {}.getType());
+        log.info("readData1={}", readData1);
+        log.info("readData2={}", readData2);
+        log.info("readData3={}", readData3);
+    }
+
+    @Test
+    void test_Gson_array() {
+        //배열 to json 문자열, 반대로
+        //
+        //요약
+        //Gson gson = new Gson()
+        //jsonString = gson.toJson(data)
+        //list = gson.fromJson(arrayString, new TypeToken<>(){}.getType())
+
+        Gson gson = new Gson();
+        List<String> data = Arrays.asList("a1", "a2", "a3");
+        String arrayString = "[\"a1\", \"a2\", \"a3\"]";
+
+        String jsonString = gson.toJson(data);
+        log.info("jsonString={}", jsonString);
+
+        //1. TypeToken 에 T 가 인텔리에서 회색 처리됨
+        List<String> list1 = gson.fromJson(arrayString, new TypeToken<List<String>>() {});
+
+        //2. new TypeToken<>(){}.getType() 을 사용하여, 제네릭 타입이 생략되는 것을 방지
+        Type listType = new TypeToken<List<String>>() {}.getType();
+        List<String> list2 = gson.fromJson(arrayString, listType);
+
+        //3. 2번을 합쳐서 사용
+        List<String> list3 = gson.fromJson(arrayString, new TypeToken<List<String>>() {}.getType());
+
+        log.info("list1={}", list1);
+        log.info("list2={}", list2);
+        log.info("list3={}", list3);
+    }
+
+    @Test
+    void test_ObjectMapper_object() throws JsonProcessingException {
+        //객체 to json 문자열
+        //반대로
+
         ObjectMapper objectMapper = new ObjectMapper();
-        Data data = createNewData();
+        Data data = createSampleData();
+        String rawString = "{\"num\":10,\"str\":\"dataHello\",\"innerData\":{\"id\":1,\"innerNum\":10,\"innerStr\":\"innerDataHello\"},\"numList\":[10,20],\"strList\":[\"a1\",\"a2\"],\"innerDataList\":[{\"id\":2,\"innerNum\":20,\"innerStr\":\"bb2\"}]}";
 
-        //object with json
-        //
-        //SerializationFeature.FAIL_ON_EMPTY_BEANS => getter 가 없는 에러
-        //
-        //{"num":10,"str":"hello","innerData":{"id":0,"innerNum":20,"innerStr":"innerHello"},"numList":[10,20,30],"strList":["a1","a2","a3"],"innerDataList":[{"id":1,"innerNum":1,"innerStr":null},{"id":2,"innerNum":2,"innerStr":null},{"id":3,"innerNum":3,"innerStr":null}]}
-        String jsonStr = objectMapper.writeValueAsString(data);
-        log.info("jsonStr={}", jsonStr);
+        //객체 to json string
+        String jsonString = objectMapper.writeValueAsString(data);
+        log.info("jsonStr={}", jsonString);
 
-        //array with json
-        //
-        //new TypeReference<>() 사용
-        //
-        //jsonArrayStr=["a1","a2","a3"]
-        List<String> stringArray = Arrays.asList("a1","a2","a3");
-        String jsonArrayStr = objectMapper.writeValueAsString(stringArray);
-        log.info("jsonArrayStr={}", jsonArrayStr);
+        //json string to 객체
+        Data readData1 = objectMapper.readValue(rawString, Data.class);
+        Data readData2 = objectMapper.readValue(rawString, new TypeReference<Data>() { });
+        Data readData3 = objectMapper.readValue(rawString, new TypeReference<>() { });
+        log.info("readData1={}", readData1);
+        log.info("readData2={}", readData2);
+        log.info("readData3={}", readData3);
 
-        String str2 = "[\"a1\", \"a2\", \"a3\"]";
-        List<String> str2List = objectMapper.readValue(
-                str2, new TypeReference<>() {
-                });
-        log.info("str2    ={}", str2);
-        log.info("str2List={}", str2List);
+        //에러 모음
+        //
+        //ObjectMapper => SerializationFeature.FAIL_ON_EMPTY_BEANS => getter 가 없을 경우 에러
+    }
+
+    @Test
+    void test_ObjectMapper_array() throws JsonProcessingException {
+        //배열 to json 문자열
+        //반대로
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> data = Arrays.asList("a1", "a2", "a3");
+        String arrayString = "[\"a1\", \"a2\", \"a3\"]";
+
+        String jsonString = objectMapper.writeValueAsString(data);
+        log.info("jsonString={}", jsonString);
+
+        //제네릭 타입 명시 안하면 경고 뜸
+        @SuppressWarnings("unchecked")
+        List<String> readData1 = objectMapper.readValue(arrayString, List.class);
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        List<String> readData2 = objectMapper.readValue(arrayString, new TypeReference<List>() {});
+
+        //제네릭 타입을 전부다 명시 하거나, 아예 생략하면 됨, 자바8 까지는 전부 명시, 11부터 생략 가능
+        List<String> readData3 = objectMapper.readValue(arrayString, new TypeReference<List<String>>() {});
+        List<String> readData4 = objectMapper.readValue(arrayString, new TypeReference<>() {
+        });
+        log.info("readData1={}", readData1);
+        log.info("readData2={}", readData2);
+        log.info("readData3={}", readData3);
+        log.info("readData4={}", readData4);
     }
 
     @Getter
     @Setter
+    @NoArgsConstructor
+    @ToString
     static class Data {
         int num;
         String str;
@@ -79,68 +164,28 @@ public class JsonTest {
 
     @Getter
     @Setter
-    @AllArgsConstructor
     @NoArgsConstructor
+    @ToString
     static class InnerData {
         long id;
         int innerNum;
         String innerStr;
+        public InnerData(long id, int innerNum, String innerStr) {
+            this.id = id;
+            this.innerNum = innerNum;
+            this.innerStr = innerStr;
+        }
     }
 
-    String str = "{\n" +
-            "    \"num\": 10,\n" +
-            "    \"str\": \"hello\",\n" +
-            "    \"innerData\": {\n" +
-            "        \"id\": 0,\n" +
-            "        \"innerNum\": 20,\n" +
-            "        \"innerStr\": \"innerHello\"\n" +
-            "    },\n" +
-            "    \"numList\": [\n" +
-            "        10,\n" +
-            "        20,\n" +
-            "        30\n" +
-            "    ],\n" +
-            "    \"strList\": [\n" +
-            "        \"a1\",\n" +
-            "        \"a2\",\n" +
-            "        \"a3\"\n" +
-            "    ],\n" +
-            "    \"innerDataList\": [\n" +
-            "        {\n" +
-            "            \"id\": 1,\n" +
-            "            \"innerNum\": 1,\n" +
-            "            \"innerStr\": \"b1\"\n" +
-            "        },\n" +
-            "        {\n" +
-            "            \"id\": 2,\n" +
-            "            \"innerNum\": 2,\n" +
-            "            \"innerStr\": \"b2\"\n" +
-            "        },\n" +
-            "        {\n" +
-            "            \"id\": 3,\n" +
-            "            \"innerNum\": 3,\n" +
-            "            \"innerStr\": \"b3\"\n" +
-            "        }\n" +
-            "    ]\n" +
-            "}";
-
-    private Data createNewData() {
-        InnerData innerData = new InnerData();
-        innerData.setId(0L);
-        innerData.setInnerNum(20);
-        innerData.setInnerStr("innerHello");
-
+    private Data createSampleData() {
         Data data = new Data();
         data.setNum(10);
-        data.setStr("hello");
-        data.setInnerData(innerData);
-        data.setNumList(Arrays.asList(10,20,30));
-        data.setStrList(Arrays.asList("a1","a2","a3"));
-        data.setInnerDataList(Arrays.asList(
-                new InnerData(1L,1,"b1"),
-                new InnerData(2L,2,"b2"),
-                new InnerData(3L,3,"b3")
-                ));
+        data.setStr("dataHello");
+        data.setInnerData(new InnerData(1L, 10, "innerDataHello"));
+        data.setNumList(Arrays.asList(10, 20));
+        data.setStrList(Arrays.asList("a1", "a2"));
+        data.setInnerDataList(List.of(
+                new InnerData(2L, 20, "bb2")));
         return data;
     }
 }
